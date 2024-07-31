c25b77bac0d71ffd5ec7fc72c47b506a5c4ce3b4
Sadik Al Jarif <sadikaljarif05@gmail.com>
2024-07-31 22:05:02 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Sadik Al Jarif";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Supervised Learning", "Regression Algorithms", "Classification Algorithms", "Unsupervised Learning", "Clustering","Anomaly Detection","Artificial Neural Networks (ANNs)","Convolutional Neural Networks (CNNs)","Recurrent Neural Networks (RNNs)","Transformer Models","Transfer Learning","MLOps Concepts","Large Language Models (LLMs)"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
